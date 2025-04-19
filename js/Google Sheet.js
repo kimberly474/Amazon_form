06f76d92acdb21e3450ca586c2d60486372b6be4
Kimberly <kimberlywoodd474@gmail.com>
2025-04-19 17:18:36 +0530

    Google Sheet.js

--- js/Google Sheet.js
+++ js/Google Sheet.js
@@ -1,7 +1,5 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyQXiDywkLYTepoVpddN3n0D9rJaQ0fLSVdNnAFUWKHI9a8Bbl_M8lkdqy1Hc0uEqAqMg/exec'
-
-const form = document.forms['amaz_form']
-
+const form = document.forms['amaz_form'];
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
